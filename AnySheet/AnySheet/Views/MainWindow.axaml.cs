7912702df5e8997b942b046fd333ea9e95fd273d
Joe Williams <hey-there-joe@outlook.com>
2026-04-22 14:57:45 -0700

    Added desktop icons.

--- AnySheet/AnySheet/Views/MainWindow.axaml.cs
+++ AnySheet/AnySheet/Views/MainWindow.axaml.cs
@@ -1,5 +1,10 @@
+using System;
+using System.Reactive;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Media;
+using Avalonia.Platform;
+using Avalonia.Styling;
 
 namespace AnySheet.Views;
 
@@ -8,5 +13,19 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        this.GetObservable(ActualThemeVariantProperty)
+            .Subscribe(new AnonymousObserver<ThemeVariant>(_ => UpdateIcon()));
+    }
+
+    /// <summary>
+    /// Whenever the window theme changes between light and dark, switches to whichever desktop icon is more visible.
+    /// </summary>
+    private void UpdateIcon()
+    {
+        // names correspond to the color of the image, *not* which theme they're used for
+        var iconName = (ActualThemeVariant == ThemeVariant.Light ? "Dark" : "Light");
+        Icon = new WindowIcon(
+            AssetLoader.Open(new Uri($"avares://AnySheet/Assets/{iconName}Icon.ico", UriKind.RelativeOrAbsolute)));
     }
 }
